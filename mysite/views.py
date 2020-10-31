2e47dbfe64a20352e10be3d3b6044863231270f0
Hung-da <C109112150@nkust.edu.tw>
2020-10-31 17:18:13 +0800

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import datetime
+import random
+
+# Create your views here.
+def index(request):
+    lucky_no = random.randint(1,49)
+    numbers = list()
+    for i in range(6):
+        numbers.append(random.randint(1,49))
+    #return HttpResponse("<h1>你今天的幸運號碼是:{}</h1>".format(lucky_no))
+    return render ( request, "index.html", locals())
+
+def date(request):
+    return JsonResponse({'date':datetime.datetime.now()})
+
+
